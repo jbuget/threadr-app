7529fb2e0b3efb4413de20d48fceacc7bf3166c0
Jérémy Buget <jbuget@gmail.com>
2023-08-29 17:44:15 +0200

    tech: add Typescript strict support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  typescript: {
+    strict: true
+  },
   css: [
     '@/assets/css/main.css',
   ],
