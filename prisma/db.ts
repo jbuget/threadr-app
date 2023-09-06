8bc9fd8a84ccde63f20fd4aa73fa47139d9657fb
Jérémy Buget <jbuget@gmail.com>
2023-09-06 22:41:40 +0200

    db: change data modelization
    No need to relational mapping. Messages and Attachments will not be updated often.
    Neither need to full-text search.
    Storing thread as-is in a JSONB column will make versionning easier.

---
+++ prisma/db.ts
@@ -0,0 +1,16 @@
+// https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+    return new PrismaClient()
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
