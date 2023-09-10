79d91a437ab43325c77ea594429f0d0840036dd2
Jérémy Buget <jbuget@gmail.com>
2023-09-10 16:46:29 +0200

    wip: the target design (that does not work)

--- app.vue
+++ app.vue
@@ -4,71 +4,23 @@ const threadId: any = ref(null)
 async function loadThread(selectedTreadId: number) {
   threadId.value = selectedTreadId
   console.log('Selected thread with ID ', threadId.value)
-  await refresh()
 }
-
-const { data, pending, refresh } = await useAsyncData(
-  async () => {
-
-    const threadsData = await $fetch('/api/threads')
-    const threads = threadsData.map((threadData: any) => ({
-      id: threadData.id,
-      createdAt: threadData.createdAt,
-      updatedAt: threadData.updatedAt,
-      published: threadData.published,
-      nbMessages: threadData.nbMessages
-    }))
-
-    let thread
-    if (threadId.value) {
-      const threadData = await $fetch(`/api/threads/${threadId.value}`, {})
-      thread = {
-        id: threadData.id,
-        messages: threadData.latest.data.messages.map((message: any) => {
-          const result = {
-            text: message.text,
-            attachments: message.attachments.map((attachment: any) => {
-              const result = {
-                filename: attachment.filename,
-                location: attachment.location,
-                mimetype: attachment.mimetype,
-                size: attachment.size,
-                alt: attachment.alt
-              }
-              return result
-            })
-          }
-          return result
-        })
-      }
-    } else {
-      thread = {
-        messages: [{
-          text: '',
-          attachments: []
-        }]
-      }
-    }
-    return { threads, thread }
-  }
-)
 
 </script>
 
 <template>
   <div class="app">
-    <div class="layout-container" v-if="data">
+    <div class="layout-container">
+      
       <div class="layout-sidebar">
-        <ThreadSummaryList :threads="data.threads" @threadSelected="loadThread" />
+        <ThreadSummaryList @threadSelected="loadThread" />
       </div>
+      
       <div class="layout-content-wrapper">
         <div class="layout-content-main">
-          <ThreadEditor :thread="data.thread" />
+          <ThreadEditor :threadId="threadId" />
         </div>
       </div>
-    </div>
-    <div v-else>
-      Loading...
     </div>
   </div>
 </template>
