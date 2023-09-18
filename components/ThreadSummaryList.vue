a7608bd55d1df79c13792b0bc23fdb1ea8e69881
Jérémy Buget <jbuget@gmail.com>
2023-09-18 23:18:19 +0200

    design: organize thread summaries by state (drafts, scheduled, published)

--- components/ThreadSummaryList.vue
+++ components/ThreadSummaryList.vue
@@ -22,7 +22,21 @@ const { data: threads } = await useFetch(
             publishedAt: threadData.publishedAt,
             nbMessages: threadData.nbMessages
         }))
-        return threads
+        const result = threads.reduce((res: any, thread: any) => {
+            if (thread.publishedAt) {
+                res.published.push(thread)
+            } else if (thread.scheduledAt) {
+                res.scheduled.push(thread)
+            } else {
+                res.drafts.push(thread)
+            }
+            return res
+        }, {
+            drafts: [],
+            scheduled: [],
+            published: []
+        })
+        return result
     }
 })
 
@@ -39,12 +53,50 @@ function badgeStyle(thread: ThreadSummary) {
 
 <template>
     <div class="threads" v-if="threads">
-        <div class="thread" v-for="(thread) in threads" @click="selectThread(thread.id)">
-            <div class="thread-meta">
-                <Badge :severity="badgeStyle(thread)"></Badge>
-                {{ (new Date(thread.createdAt)).toLocaleString() }} · {{ thread.nbMessages }} messages
+        <div class="thread-list">
+            <div class="thread-list-title">
+                <i class="pi pi-file-edit"></i>
+                <span>Drafts</span>
             </div>
-            <div class="thread-title">{{ thread.title }}</div>
+            <div class="thread-list-items">
+                <div class="thread" v-for="(thread) in threads.drafts" @click="selectThread(thread.id)">
+                    <div class="thread-meta">
+                        <Badge :severity="badgeStyle(thread)"></Badge>
+                        {{ (new Date(thread.createdAt)).toLocaleString() }} · {{ thread.nbMessages }} messages
+                    </div>
+                    <div class="thread-title">{{ thread.title }}</div>
+                </div>
+            </div>
+        </div>
+        <div class="thread-list">
+            <div class="thread-list-title">
+                <i class="pi pi-clock"></i>
+                <span>Scheduled</span>
+            </div>
+            <div class="thread-list-items">
+                <div class="thread" v-for="(thread) in threads.scheduled" @click="selectThread(thread.id)">
+                    <div class="thread-meta">
+                        <Badge :severity="badgeStyle(thread)"></Badge>
+                        {{ (new Date(thread.createdAt)).toLocaleString() }} · {{ thread.nbMessages }} messages
+                    </div>
+                    <div class="thread-title">{{ thread.title }}</div>
+                </div>
+            </div>
+        </div>
+        <div class="thread-list">
+            <div class="thread-list-title">
+                <i class="pi pi-history"></i>
+                <span>Published</span>
+            </div>
+            <div class="thread-list-items">
+                <div class="thread" v-for="(thread) in threads.published" @click="selectThread(thread.id)">
+                    <div class="thread-meta">
+                        <Badge :severity="badgeStyle(thread)"></Badge>
+                        {{ (new Date(thread.createdAt)).toLocaleString() }} · {{ thread.nbMessages }} messages
+                    </div>
+                    <div class="thread-title">{{ thread.title }}</div>
+                </div>
+            </div>
         </div>
     </div>
     <div v-else>
@@ -53,7 +105,19 @@ function badgeStyle(thread: ThreadSummary) {
 </template>
 
 <style scoped>
-.threads {
+.thread-list-title {
+    background-color: #F9F9F9;
+    padding: 0.5rem 1.5rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.thread-list-title>.pi {
+    margin-right: 0.5rem;
+}
+
+.thread-list-items {
     padding: 0.5rem;
 }
 
@@ -63,6 +127,7 @@ function badgeStyle(thread: ThreadSummary) {
     overflow: hidden;
     border-radius: 5px;
 }
+
 .thread:hover,
 .thread:focus {
     background-color: rgba(100, 116, 139, 0.04);
@@ -72,6 +137,7 @@ function badgeStyle(thread: ThreadSummary) {
     font-size: 0.8rem;
     color: grey;
 }
+
 .thread-title {
     display: inline-block;
     max-height: 2.75rem;
