83ccb9073ba7262bdb6f2dfcbf8b1929756173a0
Jérémy Buget <jbuget@gmail.com>
2023-09-18 23:58:39 +0200

    design: fix app-name link

--- app.vue
+++ app.vue
@@ -22,21 +22,21 @@ function newThread() {
     <div class="layout-container">
 
       <div class="layout-sidebar">
-        <nav class="p-fluid">
-          <div class="app-brand">
-            <span class="app-name">Threadr</span>
-          </div>
-          <div class="new-thread">
-            <Button icon="pi pi-plus" aria-label="Submit" label="New thread" severity="secondary" outlined
-              @click="newThread" @enter="newThread" />
-          </div>
+        <div class="app-brand p-fluid">
+          <a class="app-name" href="/">Threadr</a>
+        </div>
+        <div class="new-thread">
+          <Button icon="pi pi-plus" aria-label="Submit" label="New thread" severity="secondary" outlined
+            @click="newThread" @enter="newThread" />
+        </div>
+        <nav class="thread-summary-list">
           <ThreadSummaryList @threadSelected="loadThread" :key="threadSummaryListKey" />
         </nav>
       </div>
 
       <div class="layout-content-wrapper">
         <div class="layout-content-main">
-          <ThreadEditor :threadId="threadId" @threadSaved="refreshThreadList" @threadDeleted="refreshThreadList"/>
+          <ThreadEditor :threadId="threadId" @threadSaved="refreshThreadList" @threadDeleted="refreshThreadList" />
         </div>
       </div>
     </div>
@@ -51,20 +51,15 @@ function newThread() {
 }
 
 .layout-sidebar {
-  position: fixed;
-  height: 100%;
-  top: 0;
-  left: 0;
-  width: 18rem;
-  max-width: 18rem;
-  overflow-y: auto;
+  height: 100vh;
+  width: 20rem;
+  max-width: 20rem;
   border-right: 1px solid lightgray;
   display: flex;
   flex-direction: column;
 }
 
 .app-brand {
-  border-bottom: 1px solid lightgray;
   display: flex;
   align-items: center;
   height: 60px;
@@ -73,18 +68,26 @@ function newThread() {
 .app-name {
   font-weight: 800;
   font-size: 1.5rem;
+  padding: 0.5rem 1rem;
   margin: 0.5rem;
+  color: inherit;
+  text-decoration: none;
 }
 
 .new-thread {
-  margin: 0.5rem;
+  margin: 1rem;
+}
+
+.thread-summary-list {
+  overflow-y: auto;
 }
 
 .layout-content-wrapper {
-  margin-left: 18rem;
   flex-grow: 1;
   display: flex;
   flex-direction: column;
+  height: 100vh;
+  overflow-y: auto;
 }
 
 .layout-content-main {
