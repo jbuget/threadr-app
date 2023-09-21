5119475dd6e55ca4534090b3933d78711fe96a1f
Jérémy Buget <jbuget@gmail.com>
2023-09-21 19:25:33 +0200

    Add button in app.vue to open a new UserSettings component into a Dialog

---
+++ components/UserSettings.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="user-settings">
+        <div class="displaying">
+            <h3>Displaying</h3>
+        </div>
+        <div class="writing">
+            <h3>Writing</h3>
+        </div>
+        <div class="posting">
+            <h3>Posting</h3>
+        </div>
+    </div>
+</template>
+
+<style>
+.user-settings {
+    display: flex;
+    flex-direction: column; 
+    min-width: 600px;   
+}
+</style>
