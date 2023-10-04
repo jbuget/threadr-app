1984722feacfeed80f8792970980ed32b71e930b
Jérémy Buget <jbuget@gmail.com>
2023-10-05 00:40:38 +0200

    fix: BullMQ warning

--- utils/queue.ts
+++ utils/queue.ts
@@ -1,7 +1,7 @@
 import IORedis from 'ioredis';
 import { Queue } from 'bullmq';
 
-const connection = new IORedis(process.env.REDIS_URL as string);
+const connection = new IORedis(process.env.REDIS_URL as string, { maxRetriesPerRequest: null });
 
 const queue = new Queue('thread-schedules', { connection })
 
