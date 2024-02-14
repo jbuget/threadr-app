3344015811bc42194a7d955551e69f6a13565b1f
Jérémy Buget <jbuget@gmail.com>
2024-02-14 02:07:15 +0100

    Make authentication works well with Sendgrid/SMTP

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -26,10 +26,10 @@ export const authOptions: AuthConfig = {
       server: {
         host: process.env.EMAIL_SERVER_HOST,
         port: process.env.EMAIL_SERVER_PORT,
-        /*auth: {
+        auth: {
           user: process.env.EMAIL_SERVER_USER,
           pass: process.env.EMAIL_SERVER_PASSWORD
-        }*/
+        }
       },
       from: process.env.EMAIL_FROM
     }),
